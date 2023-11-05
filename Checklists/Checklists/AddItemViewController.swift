bfa858d3052e0ea738facff592f794dab4ed137c
Mittwoch <764544033@qq.com>
2023-11-05 21:33:14 +0800

    第十二章 添加项 屏幕 涉及 提交不为空

--- Checklists/Checklists/AddItemViewController.swift
+++ Checklists/Checklists/AddItemViewController.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController,UITextFieldDelegate {
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
@@ -19,6 +22,33 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done(){
+        print("Contents of the text field:\(textField.text!)")
         navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK - Table View Delegate 表格委托
+    // 表格点击禁用灰色
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    // 进入页面的时候直接给textField聚焦
+    override func viewWillAppear(_ animated: Bool) {
+        textField.becomeFirstResponder()
+    }
+    
+    // MARK: - 自己写的方法
+    // MARK: - Text Field Delegates 委托
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
     }
 }
