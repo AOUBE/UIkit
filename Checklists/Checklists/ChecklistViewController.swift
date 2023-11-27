eac0f67d973a2e87841ed51f05e3a193dba869b6
Mittwoch <764544033@qq.com>
2023-11-27 21:49:47 +0800

    第十六章 列表

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -1,169 +1,211 @@
-//
-//  ViewController.swift
-//  Checklists
-//
-//  Created by Mittwoch on 2023/11/5.
-//
+/// Copyright (c) 2021 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
 
 import UIKit
 
-class ChecklistViewController: UITableViewController,ItemDetailViewControllerDelegate {
-    func itemDetailViewControllerDidCancel(_ controller: ItemDetailViewController) {
-        navigationController?.popViewController(animated: true)
+class ChecklistViewController: UITableViewController, ItemDetailViewControllerDelegate {
+  var checklist: Checklist!
+  var items = [ChecklistItem]()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    navigationItem.largeTitleDisplayMode = .never
+    loadChecklistItems()
+    title = checklist.name
+  }
+
+  // MARK: - Navigation
+  override func prepare(
+    for segue: UIStoryboardSegue,
+    sender: Any?
+  ) {
+    if segue.identifier == "AddItem" {
+      let controller = segue.destination as! ItemDetailViewController
+      controller.delegate = self
+    } else if segue.identifier == "EditItem" {
+      let controller = segue.destination as! ItemDetailViewController
+      controller.delegate = self
+
+      if let indexPath = tableView.indexPath(
+        for: sender as! UITableViewCell) {
+        controller.itemToEdit = items[indexPath.row]
+      }
     }
-    
-    func itemDetailViewController(_ controller: ItemDetailViewController, didFinishAdding item: ChecklistItem) {
-        let newRowIndex = items.count
-        items.append(item)
-        let indexPath = IndexPath(row: newRowIndex, section: 0)
-        let indexPaths = [indexPath]
-        tableView.insertRows(at: indexPaths, with: .automatic)
-        navigationController?.popViewController(animated: true)
-        saveChecklistItems()
+  }
+
+  // MARK: - Actions
+  func configureCheckmark(
+    for cell: UITableViewCell,
+    with item: ChecklistItem
+  ) {
+    let label = cell.viewWithTag(1001) as! UILabel
+
+    if item.checked {
+      label.text = "√"
+    } else {
+      label.text = ""
     }
-    
-    func itemDetailViewController(_ controller: ItemDetailViewController, didFinishEditing item: ChecklistItem) {
-        if let index = items.firstIndex(of: item){
-            let indexPath = IndexPath(row: index, section: 0)
-            if let cell = tableView.cellForRow(at: indexPath){
-                configureText(for: cell, with: item)
-            }
-        }
-        navigationController?.popViewController(animated: true)
-        saveChecklistItems()
+  }
+
+  func configureText(
+    for cell: UITableViewCell,
+    with item: ChecklistItem
+  ) {
+    let label = cell.viewWithTag(1000) as! UILabel
+    label.text = item.text
+  }
+
+  func documentsDirectory() -> URL {
+    let paths = FileManager.default.urls(
+      for: .documentDirectory,
+         in: .userDomainMask)
+    return paths[0]
+  }
+
+  func dataFilePath() -> URL {
+    return documentsDirectory().appendingPathComponent("Checklists.plist")
+  }
+
+  func saveChecklistItems() {
+    let encoder = PropertyListEncoder()
+    do {
+      let data = try encoder.encode(items)
+      try data.write(
+        to: dataFilePath(),
+        options: Data.WritingOptions.atomic)
+    } catch {
+      print("Error encoding item array: \(error.localizedDescription)")
     }
-    
-    var items = [ChecklistItem]()
-    var row0item = ChecklistItem()
-    var row1item = ChecklistItem()
-    var row2item = ChecklistItem()
-    var row3item = ChecklistItem()
-    var row4item = ChecklistItem()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        print("Documents folder is \(documentsDirectory())")
-        print("Data file path is \(dataFilePath())")
-        loadChecklistItems()
+  }
+
+  func loadChecklistItems() {
+    let path = dataFilePath()
+    if let data = try? Data(contentsOf: path) {
+      let decoder = PropertyListDecoder()
+      do {
+        items = try decoder.decode(
+          [ChecklistItem].self,
+          from: data)
+      } catch {
+        print("Error decoding item array: \(error.localizedDescription)")
+      }
     }
-    
-    // MARK: - Table View Data Source 表格数据源
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+  }
+
+  // MARK: - Table View Data Source
+  override func tableView(
+    _ tableView: UITableView,
+    numberOfRowsInSection section: Int
+  ) -> Int {
+    return items.count
+  }
+
+  override func tableView(
+    _ tableView: UITableView,
+    cellForRowAt indexPath: IndexPath
+  ) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(
+      withIdentifier: "ChecklistItem",
+      for: indexPath)
+
+    let item = items[indexPath.row]
+
+    configureText(for: cell, with: item)
+    configureCheckmark(for: cell, with: item)
+
+    return cell
+  }
+
+  // MARK: - Table View Delegate
+  override func tableView(
+    _ tableView: UITableView,
+    didSelectRowAt indexPath: IndexPath
+  ) {
+    if let cell = tableView.cellForRow(at: indexPath) {
+      let item = items[indexPath.row]
+      item.checked.toggle()
+
+      configureCheckmark(for: cell, with: item)
     }
-    // cellForRowAt 外部名称。indexPath：内部名称
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
-        let item = items[indexPath.row]
+
+    tableView.deselectRow(at: indexPath, animated: true)
+    saveChecklistItems()
+  }
+
+  override func tableView(
+    _ tableView: UITableView,
+    commit editingStyle: UITableViewCell.EditingStyle,
+    forRowAt indexPath: IndexPath
+  ) {
+    items.remove(at: indexPath.row)
+
+    let indexPaths = [indexPath]
+    tableView.deleteRows(at: indexPaths, with: .automatic)
+    saveChecklistItems()
+  }
+
+  // MARK: - Add Item ViewController Delegates
+  func itemDetailViewControllerDidCancel(
+    _ controller: ItemDetailViewController
+  ) {
+    navigationController?.popViewController(animated: true)
+  }
+
+  func itemDetailViewController(
+    _ controller: ItemDetailViewController,
+    didFinishAdding item: ChecklistItem
+  ) {
+    let newRowIndex = items.count
+    items.append(item)
+
+    let indexPath = IndexPath(row: newRowIndex, section: 0)
+    let indexPaths = [indexPath]
+    tableView.insertRows(at: indexPaths, with: .automatic)
+    navigationController?.popViewController(animated:true)
+    saveChecklistItems()
+  }
+
+  func itemDetailViewController(
+    _ controller: ItemDetailViewController,
+    didFinishEditing item: ChecklistItem
+  ) {
+    if let index = items.firstIndex(of: item) {
+      let indexPath = IndexPath(row: index, section: 0)
+      if let cell = tableView.cellForRow(at: indexPath) {
         configureText(for: cell, with: item)
-        configureCheckmark(for: cell, with: item)
-        return cell
+      }
     }
-    
-    // MARK - Table View Delegate 表格委托
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let cell = tableView.cellForRow(at: indexPath){
-            let item = items[indexPath.row]
-            item.checked.toggle()
-            configureCheckmark(for: cell, with: item)
-        }
-        tableView.deselectRow(at: indexPath, animated: true)
-        saveChecklistItems()
-    }
-    
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        items.remove(at: indexPath.row)
-        let indexPaths = [indexPath]
-        tableView.deleteRows(at: indexPaths, with: .automatic)
-        saveChecklistItems()
-    }
-    
-    // MARK: - 自己写的方法
-    // 修改行的选择状态
-    func configureCheckmark(
-        for cell : UITableViewCell,
-        with item : ChecklistItem
-    ){
-        let label = cell.viewWithTag(1001) as! UILabel
-        if item.checked{
-            label.text = "✔️"
-        } else {
-            label.text = ""
-        }
-    }
-    
-    //修改行的文字
-    func configureText(
-        for cell : UITableViewCell,
-        with item : ChecklistItem
-    ){
-        let label = cell.viewWithTag(1000) as! UILabel
-        label.text = item.text
-    }
-    
-    // MARK: - Actions
-    //    @IBAction func addItem(){
-    //        let newRowIndex = items.count
-    //
-    //        let item = ChecklistItem()
-    //        item.text = "I am a new row"
-    //        items.append(item)
-    //
-    //        let indexPath = IndexPath(row:newRowIndex,section: 0)
-    //        let indexPaths = [indexPath]
-    //        tableView.insertRows(at: indexPaths, with: .automatic)
-    //    }
-    
-    // MARK: - Navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "AddItem" {
-            let controller = segue.destination as! ItemDetailViewController
-            controller.delegate = self
-        } else if segue.identifier == "EditItem" {
-            let controller = segue.destination as! ItemDetailViewController
-            controller.delegate = self
-            if let indexPath = tableView.indexPath(for: sender as! UITableViewCell ){
-                controller.itemToEdit = items[indexPath.row]
-            }
-        }
-    }
-    
-    
-    // MARK: - 数据持久型
-    func documentsDirectory() -> URL{
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-    
-    func dataFilePath() -> URL {
-      return documentsDirectory().appendingPathComponent("Checklists.plist")
-    }
-    
-    //存储文件
-    func saveChecklistItems() {
-        let encoder = PropertyListEncoder()
-        do {
-            let data = try encoder.encode(items)
-            try data.write(to: dataFilePath(), options: Data.WritingOptions.atomic)
-        } catch {
-            print("Error encoding item array : \(error.localizedDescription)")
-        }
-    }
-    
-    //加载文件
-    func loadChecklistItems() {
-        let path = dataFilePath()
-        if let data = try? Data(contentsOf: path) {
-            let decoder = PropertyListDecoder()
-            do {
-                items = try decoder.decode([ChecklistItem].self, from: data)
-            } catch {
-                print("Error decoding item array : \(error.localizedDescription)")
-            }
-        }
-    }
+    navigationController?.popViewController(animated: true)
+    saveChecklistItems()
+  }
 }
+
