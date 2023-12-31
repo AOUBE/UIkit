14a2c82b33df21671a6f66e7cdf4bfe5f9c87d07
Mittwoch <764544033@qq.com>
2023-12-31 16:23:16 +0800

    第三个应用新建

---
+++ MyLocations/MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Mittwoch on 2023/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
