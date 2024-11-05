3029264bca9bdee74c6fecd1143d0fe8c661ba5c
twilight <764544033@qq.com>
2024-11-05 16:58:04 +0800

    Chapter 9: Table Views——END

---
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by mittwoch on 2024/11/4.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Table View Data Source 数据源
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //dequeueReusableCell(withIdentifier: <#T##String#>, for: <#T##IndexPath#>)得到了单元格的副本
+        //indexPath 一个指向表中特定行的对象
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        //向表格视图单元格询问带有标签1000的视图
+        let label  = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn ios development"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Soccer practice"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Eat ice cream"
+        }
+        
+        return cell
+    }
+    
+    // MARK: - Table View Delegate 委托
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath){
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
