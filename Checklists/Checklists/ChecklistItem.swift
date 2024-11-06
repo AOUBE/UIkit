31bbc211c6c0544aff100f03120a8c3d16664e91
twilight <764544033@qq.com>
2024-11-06 17:47:48 +0800

    Chapter 10: The Data Model——END

---
+++ Checklists/Checklists/ChecklistItem.swift
@@ -0,0 +1,12 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by mittwoch on 2024/11/6.
+//
+
+import Foundation
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
