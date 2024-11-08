7e05b064f0388dd1d9ddf9b9273dfe45c6fc3cb3
twilight <764544033@qq.com>
2024-11-08 16:54:40 +0800

    Chapter 11: Navigation Controllers——END

---
+++ Checklists/Checklists/AddItemViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by mittwoch on 2024/11/8.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    //MARK: - Actions
+    @IBAction func cancel(){
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done(){
+        navigationController?.popViewController(animated: true)
+    }
+}
--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -16,6 +16,7 @@ class ChecklistViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        navigationController?.navigationBar.prefersLargeTitles = true
         
         // Add the following lines
         let item1 = ChecklistItem()
@@ -57,16 +58,35 @@ class ChecklistViewController: UITableViewController {
     }
     
     // MARK: - Table View Delegate 委托
+    //这个方法修改了表格的选择按钮
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at: indexPath){
             let item = items[indexPath.row]
             item.checked.toggle()
             configureCheckmark(for: cell, with: item)
         }
+        //修改了表格选中状态
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    //这个方法是滑动删除的方法
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        items.remove(at: indexPath.row)
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
     
-    
+    // MARK: - Actions
+    @IBAction func addItem(){
+        let newRowIndex = items.count
+        
+        let item = ChecklistItem()
+        item.text = "I am a new row"
+        items.append(item)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
     
     // MARK: - function
     func configureCheckmark(
@@ -84,7 +104,7 @@ class ChecklistViewController: UITableViewController {
         for cell: UITableViewCell,
         with item: ChecklistItem
     ) {
-        ////向表格视图单元格询问带有标签1000的视图
+        //向表格视图单元格询问带有标签1000的视图
         let label = cell.viewWithTag(1000) as! UILabel
         label.text = item.text
     }
