fd6a2614ce8c74183582e9d8009583b7d881d63c
twilight <764544033@qq.com>
2024-11-08 22:33:03 +0800

    Chapter 12: Add Item Screen——END

--- Checklists/Checklists/AddItemViewController.swift
+++ Checklists/Checklists/AddItemViewController.swift
@@ -7,12 +7,37 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController,UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var textField: UITextField!
+    
+    //MARK: - Table View Delegates
+    //禁止此行被选中
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    //MARK: - Text Field Delegates
+    // textField(_:shouldChangeCharactersIn:replacementString:) 不会提供新文本，只为你提供文本的那一部分需要替换
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText =  textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        // 这个是用来判断是否为空的
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
+    //textfield 清除的时候会调用的方法
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
     }
     
     //MARK: - Actions
@@ -21,6 +46,15 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done(){
+        print("Contents of the text field: \(textField.text!)")
+        
         navigationController?.popViewController(animated: true)
+    }
+    
+    //MARK: - function
+    //textField 聚焦
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
 }
